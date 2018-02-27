c3be1ec27ace63070a926da650157af70f4290de
Alexey <tiko1vg@gmail.com>
2018-02-27 23:14:02 +0300

    Implement dynamic linked list

---
+++ data-structures/linked_list.py
@@ -0,0 +1,26 @@
+class LinkedList:
+
+    head = None
+    first = None
+    last = None
+
+    def add(self, value):
+        _node = Node(value)
+
+        if not self.first:
+            _node.next = None
+            self.head = _node
+            self.last = _node
+            self.first = _node
+            return
+
+        self.last.next = _node
+
+
+class Node:
+    value = None
+    next = None
+
+    def __init__(self, value):
+        self.value = value
+
